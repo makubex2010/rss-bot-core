2fae3664b6d5231813f426518139bac87f897507
KevinChen <67411136+makubex2010@users.noreply.github.com>
2022-02-27 22:23:07 +0800

    Update export.go

--- internal/bot/handler/export.go
+++ internal/bot/handler/export.go
@@ -22,7 +22,7 @@ func NewExport() *Export {
 }
 
 func (e *Export) Description() string {
-	return "导出opml"
+	return "導出opml"
 }
 
 func (e *Export) Command() string {
@@ -38,15 +38,15 @@ func (e *Export) getChatSources(id int64) ([]model.Source, error) {
 }
 
 func (e *Export) getChannelSources(bot *tb.Bot, opUserID int64, channelName string) ([]model.Source, error) {
-	// 导出channel订阅
+	// 導出channel訂閱
 	channelChat, err := bot.ChatByUsername(channelName)
 	if err != nil {
-		return nil, errors.New("无法获取频道信息")
+		return nil, errors.New("無法獲取頻道信息")
 	}
 
 	adminList, err := bot.AdminsOf(channelChat)
 	if err != nil {
-		return nil, errors.New("无法获取频道管理员信息")
+		return nil, errors.New("無法獲取頻道管理員資訊")
 	}
 
 	senderIsAdmin := false
@@ -58,13 +58,13 @@ func (e *Export) getChannelSources(bot *tb.Bot, opUserID int64, channelName stri
 	}
 
 	if !senderIsAdmin {
-		return nil, errors.New("非频道管理员无法执行此操作")
+		return nil, errors.New("非頻道管理員無法執行此操作")
 	}
 
 	sources, err := e.getChatSources(channelChat.ID)
 	if err != nil {
 		zap.S().Error(err)
-		return nil, errors.New("获取订阅源信息失败")
+		return nil, errors.New("獲取訂閱源信息失敗")
 	}
 	return sources, nil
 }
@@ -77,7 +77,7 @@ func (e *Export) Handle(ctx tb.Context) error {
 		sourceList, err = e.getChatSources(ctx.Chat().ID)
 		if err != nil {
 			zap.S().Warnf(err.Error())
-			return ctx.Send("导出失败")
+			return ctx.Send("匯出失敗")
 		}
 	} else {
 		var err error
@@ -89,18 +89,18 @@ func (e *Export) Handle(ctx tb.Context) error {
 	}
 
 	if len(sourceList) == 0 {
-		return ctx.Send("订阅列表为空")
+		return ctx.Send("訂閱列表為空")
 	}
 
 	opmlStr, err := opml.ToOPML(sourceList)
 	if err != nil {
-		return ctx.Send("导出失败")
+		return ctx.Send("匯出失敗")
 	}
 	opmlFile := &tb.Document{File: tb.FromReader(strings.NewReader(opmlStr))}
 	opmlFile.FileName = fmt.Sprintf("subscriptions_%d.opml", time.Now().Unix())
 	if err := ctx.Send(opmlFile); err != nil {
 		zap.S().Errorf("send opml file failed, err:%+v", err)
-		return ctx.Send("导出失败")
+		return ctx.Send("匯出失敗")
 	}
 	return nil
 }
