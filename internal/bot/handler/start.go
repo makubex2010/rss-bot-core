dae9b57b261e5573b26b8281009c94e72500db17
KevinChen <67411136+makubex2010@users.noreply.github.com>
2022-02-27 22:31:51 +0800

    Update start.go

--- internal/bot/handler/start.go
+++ internal/bot/handler/start.go
@@ -21,15 +21,16 @@ func (s *Start) Command() string {
 }
 
 func (s *Start) Description() string {
-	return "开始使用"
+	return "開始使用"
 }
 
 func (s *Start) Handle(ctx tb.Context) error {
 	user, _ := model.FindOrCreateUserByTelegramID(ctx.Chat().ID)
 	zap.S().Infof("/start user_id: %d telegram_id: %d", user.ID, user.TelegramID)
-	return ctx.Send(fmt.Sprintf("你好，欢迎使用flowerss。"))
+	return ctx.Send(fmt.Sprintf("你好，歡迎使用無料案內所RSSBOT。"))
 }
 
 func (s *Start) Middlewares() []tb.MiddlewareFunc {
 	return nil
 }
+
