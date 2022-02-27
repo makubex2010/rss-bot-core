cdf5219105d13f7774f4c4e0a7ae0daff23d9c21
KevinChen <67411136+makubex2010@users.noreply.github.com>
2022-02-27 22:28:03 +0800

    Update remove_subscription.go

--- internal/bot/handler/remove_subscription.go
+++ internal/bot/handler/remove_subscription.go
@@ -26,22 +26,22 @@ func (s *RemoveSubscription) Command() string {
 }
 
 func (s *RemoveSubscription) Description() string {
-	return "退订RSS源"
+	return "退訂RSS源"
 }
 
 func (s *RemoveSubscription) removeForChannel(ctx tb.Context, channelName string) error {
 	sourceURL := message.URLFromMessage(ctx.Message())
 	if sourceURL == "" {
-		return ctx.Send("频道退订请使用' /unsub @ChannelID URL ' 命令")
+		return ctx.Send("頻道退訂請使用' /unsub @ChannelID URL ' 命令")
 	}
 
 	channelChat, err := s.bot.ChatByUsername(channelName)
 	if err != nil {
-		return ctx.Reply("获取频道信息错误")
+		return ctx.Reply("獲取頻道資訊錯誤")
 	}
 
 	if !chat.IsChatAdmin(s.bot, channelChat, ctx.Sender().ID) {
-		return ctx.Reply("非频道管理员无法执行此操作")
+		return ctx.Reply("非頻道管理員無法執行此操作")
 	}
 
 	source, _ := model.GetSourceByUrl(sourceURL)
@@ -49,14 +49,14 @@ func (s *RemoveSubscription) removeForChannel(ctx tb.Context, channelName string
 	if err != nil {
 		if err.Error() == "record not found" {
 			return ctx.Send(
-				fmt.Sprintf("频道 [%s](https://t.me/%s) 未订阅该RSS源", channelChat.Title, channelChat.Username),
+				fmt.Sprintf("頻道 [%s](https://t.me/%s) 未訂閱該RSS源", channelChat.Title, channelChat.Username),
 				&tb.SendOptions{
 					DisableWebPagePreview: true,
 					ParseMode:             tb.ModeMarkdown,
 				},
 			)
 		}
-		return ctx.Reply("退订失败")
+		return ctx.Reply("退訂失敗")
 	}
 	zap.S().Infof("%d for [%d]%s unsubscribe %s", ctx.Chat().ID, source.ID, source.Title, source.Link)
 	if err := sub.Unsub(); err != nil {
@@ -64,11 +64,11 @@ func (s *RemoveSubscription) removeForChannel(ctx tb.Context, channelName string
 			"%d for [%d]%s unsubscribe %s failed, %v",
 			ctx.Chat().ID, source.ID, source.Title, source.Link, err,
 		)
-		return ctx.Reply("退订失败")
+		return ctx.Reply("退訂失敗")
 	}
 	return ctx.Send(
 		fmt.Sprintf(
-			"频道 [%s](https://t.me/%s) 退订 [%s](%s) 成功",
+			"頻道 [%s](https://t.me/%s) 退訂 [%s](%s) 成功",
 			channelChat.Title, channelChat.Username, source.Title, source.Link,
 		),
 		&tb.SendOptions{DisableWebPagePreview: true, ParseMode: tb.ModeMarkdown},
@@ -80,18 +80,18 @@ func (s *RemoveSubscription) removeForChat(ctx tb.Context) error {
 	if sourceURL == "" {
 		subs, err := model.GetSubsByUserID(ctx.Chat().ID)
 		if err != nil {
-			return ctx.Reply("获取订阅列表失败")
+			return ctx.Reply("獲取訂閱列表失敗")
 		}
 
 		if len(subs) == 0 {
-			return ctx.Reply("没有订阅")
+			return ctx.Reply("沒有訂閱")
 		}
 
 		var unsubFeedItemButtons [][]tb.InlineButton
 		for _, sub := range subs {
 			source, err := model.GetSourceById(sub.SourceID)
 			if err != nil {
-				return ctx.Reply("获取订阅列表失败")
+				return ctx.Reply("獲取訂閱列表失敗")
 			}
 			unsubFeedItemButtons = append(
 				unsubFeedItemButtons, []tb.InlineButton{
@@ -103,16 +103,16 @@ func (s *RemoveSubscription) removeForChat(ctx tb.Context) error {
 				},
 			)
 		}
-		return ctx.Reply("请选择你要退订的源", &tb.ReplyMarkup{InlineKeyboard: unsubFeedItemButtons})
+		return ctx.Reply("請選擇你要退訂的源", &tb.ReplyMarkup{InlineKeyboard: unsubFeedItemButtons})
 	}
 
 	if !chat.IsChatAdmin(s.bot, ctx.Chat(), ctx.Sender().ID) {
-		return ctx.Reply("非管理员无法执行此操作")
+		return ctx.Reply("非管理員無法執行此操作")
 	}
 
 	source, err := model.GetSourceByUrl(sourceURL)
 	if err != nil || source == nil {
-		return ctx.Reply("未订阅该RSS源")
+		return ctx.Reply("未訂閱該RSS源")
 	}
 
 	zap.S().Infof("%d unsubscribe [%d]%s %s", ctx.Chat().ID, source.ID, source.Title, source.Link)
@@ -121,10 +121,10 @@ func (s *RemoveSubscription) removeForChat(ctx tb.Context) error {
 			"%d for [%d]%s unsubscribe %s failed, %v",
 			ctx.Chat().ID, source.ID, source.Title, source.Link, err,
 		)
-		return ctx.Reply("退订失败")
+		return ctx.Reply("退訂失敗")
 	}
 	return ctx.Send(
-		fmt.Sprintf("[%s](%s) 退订成功！", source.Title, source.Link),
+		fmt.Sprintf("[%s](%s) 退訂成功！", source.Title, source.Link),
 		&tb.SendOptions{DisableWebPagePreview: true, ParseMode: tb.ModeMarkdown},
 	)
 }
@@ -162,12 +162,12 @@ func (r *RemoveSubscriptionItemButton) Description() string {
 
 func (r *RemoveSubscriptionItemButton) Handle(ctx tb.Context) error {
 	if ctx.Callback() == nil {
-		return ctx.Edit("内部错误！")
+		return ctx.Edit("內部錯誤！")
 	}
 
 	data := strings.Split(ctx.Callback().Data, ":")
 	if len(data) != 3 {
-		return ctx.Edit("退订错误！")
+		return ctx.Edit("退訂錯誤！")
 	}
 
 	userID, _ := strconv.Atoi(data[0])
@@ -175,17 +175,18 @@ func (r *RemoveSubscriptionItemButton) Handle(ctx tb.Context) error {
 	sourceID, _ := strconv.Atoi(data[2])
 	source, err := model.GetSourceById(uint(sourceID))
 	if err != nil {
-		return ctx.Edit("退订错误！")
+		return ctx.Edit("退訂錯誤！")
 	}
 
 	if err := model.UnsubByUserIDAndSubID(int64(userID), uint(subID)); err != nil {
-		return ctx.Edit("退订错误！")
+		return ctx.Edit("退訂錯誤！")
 	}
 
-	rtnMsg := fmt.Sprintf("[%d] <a href=\"%s\">%s</a> 退订成功", sourceID, source.Link, source.Title)
+	rtnMsg := fmt.Sprintf("[%d] <a href=\"%s\">%s</a> 退訂成功", sourceID, source.Link, source.Title)
 	return ctx.Edit(rtnMsg, &tb.SendOptions{ParseMode: tb.ModeHTML})
 }
 
 func (r *RemoveSubscriptionItemButton) Middlewares() []tb.MiddlewareFunc {
 	return nil
 }
+
