549167fd4bf21d1a2ef54f9ac155ef74b383c326
KevinChen <67411136+makubex2010@users.noreply.github.com>
2022-02-27 22:25:02 +0800

    Update list_subscription.go

--- internal/bot/handler/list_subscription.go
+++ internal/bot/handler/list_subscription.go
@@ -22,31 +22,31 @@ func (l *ListSubscription) Command() string {
 }
 
 func (l *ListSubscription) Description() string {
-	return "已订阅的RSS源"
+	return "已訂閱的RSS源"
 }
 
 func (l *ListSubscription) listChatSubscription(ctx tb.Context) error {
 	// private chat or group
 	if ctx.Chat().Type != tb.ChatPrivate && !chat.IsChatAdmin(ctx.Bot(), ctx.Chat(), ctx.Sender().ID) {
-		// 无权限
-		return ctx.Send("无权限")
+		// 無許可權
+		return ctx.Send("無許可權")
 	}
 
 	user, err := model.FindOrCreateUserByTelegramID(ctx.Chat().ID)
 	if err != nil {
-		return ctx.Send("获取频道订阅错误")
+		return ctx.Send("獲取頻道訂閱錯誤")
 	}
 
 	subSourceMap, err := user.GetSubSourceMap()
 	if err != nil {
-		return ctx.Send("获取频道订阅错误")
+		return ctx.Send("獲取頻道訂閱錯誤")
 	}
 
 	if len(subSourceMap) == 0 {
-		return ctx.Send("订阅列表为空")
+		return ctx.Send("訂閱列表為空")
 	}
 
-	rspMessage := "当前订阅列表：\n"
+	rspMessage := "當前訂閱列表：\n"
 	for sub, source := range subSourceMap {
 		rspMessage = rspMessage + fmt.Sprintf("[[%d]] [%s](%s)\n", sub.ID, source.Title, source.Link)
 	}
@@ -58,30 +58,30 @@ func (l *ListSubscription) listChatSubscription(ctx tb.Context) error {
 func (l *ListSubscription) listChannelSubscription(ctx tb.Context, channelName string) error {
 	channelChat, err := ctx.Bot().ChatByUsername(channelName)
 	if err != nil {
-		return ctx.Send("获取频道信息错误")
+		return ctx.Send("獲取頻道資訊錯誤")
 	}
 
 	if !chat.IsChatAdmin(ctx.Bot(), channelChat, ctx.Sender().ID) {
-		return ctx.Send("非频道管理员无法执行此操作")
+		return ctx.Send("非頻道管理員無法執行此操作")
 	}
 
 	user, err := model.FindOrCreateUserByTelegramID(channelChat.ID)
 	if err != nil {
-		return ctx.Send("获取频道订阅错误")
+		return ctx.Send("獲取頻道訂閱錯誤")
 	}
 
 	subSourceMap, err := user.GetSubSourceMap()
 	if err != nil {
-		return ctx.Send("获取频道订阅错误")
+		return ctx.Send("獲取頻道訂閱錯誤")
 	}
 	if len(subSourceMap) == 0 {
 		return ctx.Send(
-			fmt.Sprintf("频道 [%s](https://t.me/%s) 订阅列表为空", channelChat.Title, channelChat.Username),
+			fmt.Sprintf("頻道 [%s](https://t.me/%s) 訂閱列表為空", channelChat.Title, channelChat.Username),
 			&tb.SendOptions{DisableWebPagePreview: true, ParseMode: tb.ModeMarkdown},
 		)
 	}
 
-	rspMessage := fmt.Sprintf("频道 [%s](https://t.me/%s) 订阅列表：\n", channelChat.Title, channelChat.Username)
+	rspMessage := fmt.Sprintf("頻道 [%s](https://t.me/%s) 訂閱列表：\n", channelChat.Title, channelChat.Username)
 	for sub, source := range subSourceMap {
 		rspMessage = rspMessage + fmt.Sprintf("[[%d]] [%s](%s)\n", sub.ID, source.Title, source.Link)
 	}
